21e2be646d23cd13992064ed2e18dc78214f7636
gulli301185 <gulli301185@gmail.com>
2024-11-07 16:43:37 +0600

    made login

---
+++ src/components/Login.jsx
@@ -0,0 +1,37 @@
+export const Login = () => {
+  return (
+    <div className="h-screen  flex justify-center items-center">
+      <div className="bg-pink-300 p-8 space-y-8 rounded-lg">
+        <h1 className="text-3xl text-gray-700 font-bold text-center pb-10">
+          Login
+        </h1>
+        <div className="flex flex-col">
+          <label htmlFor="email" className="text-lg text-gray-800">
+            email
+          </label>
+          <input
+            type="text"
+            id="email"
+            className="py-2 pr-52 pl-2 rounded-md shadow-2xl ring-1 ring-slate-600"
+            placeholder="email"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="password" className="text-lg text-gray-800">
+            password
+          </label>
+          <input
+            type="text"
+            id="password"
+            className="py-2 pr-52 pl-2 rounded-md shadow-2xl ring-1 ring-slate-600"
+            placeholder="password"
+          />
+        </div>
+
+        <button className="bg-pink-400 py-2 w-full rounded-md shadow-pink-950 shadow-2xl text-lg text-pink-950 leading-loose font-bold">
+          Register
+        </button>
+      </div>
+    </div>
+  );
+};
