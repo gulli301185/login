a3e7db09f80d83b9f2481bb0867195b0860eb03f
gulli301185 <gulli301185@gmail.com>
2024-11-07 18:07:59 +0600

    fixed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,18 @@
 export const Login = () => {
   return (
-    <div className="h-screen  flex justify-center items-center">
-      <div className="bg-pink-300 px-2 py-8 space-y-8 rounded-lg">
+    <div className="h-screen container min-w-full  flex justify-center items-center">
+      <div className="w-full mx-auto bg-pink-300 px-2 py-8 space-y-8 rounded-lg">
         <h1 className="text-sm lg:text-3xl text-gray-700 font-bold text-center pb-0 lg:pb-8">
           Login
         </h1>
-        <div className="flex flex-col w-52 lg:w-96">
+        <div className="flex flex-col ">
           <label htmlFor="email" className="text-lg text-gray-800">
             email
           </label>
           <input
             type="text"
             id="email"
-            className="py-2  pl-2 w-full rounded-md shadow-2xl ring-1 ring-slate-600"
+            className="py-2  pl-2 min-w-full  rounded-md shadow-2xl ring-1 ring-slate-600"
             placeholder="email"
           />
         </div>
@@ -23,12 +23,12 @@ export const Login = () => {
           <input
             type="text"
             id="password"
-            className="py-2  pl-2 w-full rounded-md shadow-2xl ring-1 ring-slate-600"
+            className="py-2  pl-2 min-w-full  rounded-md shadow-2xl ring-1 ring-slate-600"
             placeholder="password"
           />
         </div>
 
-        <button className="bg-pink-400 py-2 w-full rounded-md shadow-pink-950 shadow-2xl text-lg text-pink-950 leading-loose font-bold">
+        <button className="bg-pink-400 py-2 min-w-full rounded-md shadow-pink-950 shadow-2xl text-lg text-pink-950 leading-loose font-bold">
           Register
         </button>
       </div>
